39af36d5fc0363182e3daf99e7647c2d83f68028
Marlon Almino <alminomarlon@gmail.com>
2025-05-20 21:50:14 -0300

    collectstatic no render free plan

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,6 @@ TEMPLATES = [{
 }]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-STATIC_URL = '/static/'
 
 DATABASES = {
   "default": dj_database_url.config(default=os.getenv("DATABASE_URL"), conn_max_age=600, ssl_require=True)
